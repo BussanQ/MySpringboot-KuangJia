09daf72a9d0441689e21b0b5f5d85d2094c373f9
MangaQQQ <11120888@bjtu.edu.cn>
2019-06-25 00:28:27 +0800

    springfinal修改

--- src/main/java/com/bussanq/group/alpha/Application.java
+++ src/main/java/com/bussanq/group/alpha/Application.java
@@ -1,6 +1,7 @@
 package com.bussanq.group.alpha;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,13 +12,14 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 //@MapperScan(basePackages = "com.bussanq.group.alpha.mapper")
 public class Application implements CommandLineRunner{
 
-	Logger logger = Logger.getLogger(Application.class);
+	Logger logger = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(Application.class, args);
 	}
 
 	@Override
-	public void run(String... strings) throws Exception {
+	public void run(String... strings) {
+		logger.info("初始化完成");
 	}
 }
--- src/main/java/com/bussanq/group/alpha/common/conf/WebConfig.java
+++ src/main/java/com/bussanq/group/alpha/common/conf/WebConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,7 +20,7 @@ import java.util.List;
  * @date 2017/11/20
  */
 @Configuration
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		//将所有/static/** 访问都映射到classpath:/static/ 目录下
